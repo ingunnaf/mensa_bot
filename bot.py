9ad5c9af73d56dce66769c35b82ef0dcb8886da5
Ingunn Augdal Fløvig <ingunn.flovig@gmail.com>
2023-03-05 23:00:57 +0100

    Cleaned up comments and names

--- bot.py
+++ bot.py
@@ -2,35 +2,21 @@ import os
 import telebot
 import mensa_scraper as scraper
 
-
-
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 
-
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
-
-@bot.message_handler(commands=['fetch', 'new'])
-def fetch_new_menu(message):
-    scraper.downloadMenuPDF()
-    bot.reply_to(message, "I fetched the new menu for you!")
-
+    bot.reply_to(message, "hello")
 
 @bot.message_handler(commands=['menu', 'food'])
 def get_menu(message):
     pdfurl = scraper.getPDFurl()
     bot.send_document(message.chat.id, pdfurl)
-    #bot.send_document(message, "FILEID")
+
+@bot.message_handler(commands=['fetch', 'new'])
+def download_new_menu(message):
+    scraper.downloadMenuPDF()
+    bot.reply_to(message, "I downloaded the new menu!")
 
 bot.infinity_polling()
-
-
-"""f = open("/menu.pdf", 'rb')
-    file_bytes = f.read()
-    f.close()
-
-    response = {
-        'document': (f.name, file_bytes)
-    }"""
--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ import pandas as pd
 import tabula as tb
 import textract as tx
 from PyPDF2 import PdfReader
-url = "https://www.stw.berlin/mensen/einrichtungen/technische-universität-berlin/mensa-tu-hardenbergstraße.html"
+mensaURL = "https://www.stw.berlin/mensen/einrichtungen/technische-universität-berlin/mensa-tu-hardenbergstraße.html"
 
 def getPDFurl(): 
 
@@ -12,40 +12,29 @@ def getPDFurl():
     url='https://proxy.scrapeops.io/v1/',
     params={
         'api_key': '904703d4-06e0-466f-a6cd-d030a70e39bd',
-        'url': url, 
+        'url': mensaURL, 
     },
   )
 
   soup = BeautifulSoup(page.content, 'html.parser')
   speiseplan = soup.find('div', id='speiseplan')
-  file = speiseplan.find('a')
-  file = 'https://www.stw.berlin' + file.attrs['href']
-  return file
+  menuURL = speiseplan.find('a')
+  menuURL = 'https://www.stw.berlin' + menuURL.attrs['href']
+  return menuURL
 
 def downloadMenuPDF():
-  file = getPDFurl()
+  menuURL = getPDFurl()
 
   page2 = requests.get(
     url='https://proxy.scrapeops.io/v1/',
     params={
         'api_key': '904703d4-06e0-466f-a6cd-d030a70e39bd',
-        'url': file, 
+        'url': menuURL, 
     },
   )
 
   pdf = open("menu.pdf", 'wb')
   pdf.write(page2.content)
   pdf.close()
-  
 
-#reader = PdfReader("menu.pdf")
-#number_of_pages = len(reader.pages)
-#page = reader.pages[1]
-#text = page.extract_text()
-#print(text)
-
-## Now I have the pdf of the weekly plan for the food :-) 
-## Now I can extract the info for one particular day from the pdf. 
-
-# How do we do that ? And send it to a telegram bot ? 
 
